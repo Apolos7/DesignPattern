82f616fb584d95ebf577d0bcc77f62b89190298c
Kamille Gomes <kamillegobe2807@gmail.com>
2022-07-17 01:03:02 -0300

    correcão de espaços

--- src/designpatterns/observer/subject/Subject.java
+++ src/designpatterns/observer/subject/Subject.java
@@ -2,7 +2,6 @@ package designpatterns.observer.subject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import designpatterns.observer.observers.Observer;
